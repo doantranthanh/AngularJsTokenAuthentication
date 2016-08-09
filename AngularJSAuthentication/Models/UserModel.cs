5deafb2d004a514a2042bce88c0101992830dca3
Thanh Doan <Thanh Doan@xlntelecom.local>
2016-08-09 15:08:50 +0100

    Adding Startup, AuthContext, UserModel classes

---
+++ AngularJSAuthentication/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJSAuthentication.API.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The password and confirmation password do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
