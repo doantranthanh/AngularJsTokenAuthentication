f78883ca07a863caff352b49d7ff69036969a6a1
Thanh Doan <Thanh Doan@xlntelecom.local>
2016-08-09 16:06:17 +0100

    Init Startup class

---
+++ AngularJSAuthentication/AngularJSAuthentication.API/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly:OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
+namespace AngularJSAuthentication.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
