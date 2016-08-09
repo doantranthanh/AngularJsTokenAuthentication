c94aa157fba322891354970ea429e9cb9a111500
Thanh Doan <Thanh Doan@xlntelecom.local>
2016-08-09 16:45:51 +0100

    Implement Token service in Startup class

--- AngularJSAuthentication/AngularJSAuthentication.API/Startup.cs
+++ AngularJSAuthentication/AngularJSAuthentication.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly:OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
@@ -14,9 +15,28 @@ namespace AngularJSAuthentication.API
     {
         public void Configuration(IAppBuilder app)
         {
+            
             HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
